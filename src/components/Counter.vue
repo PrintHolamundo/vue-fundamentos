f6e73789b58a2063f9bbd32debfeeaa0017d8f80
PrintHolamundo <jdohca@gmail.com>
2022-03-11 21:52:01 -0600

    Propiedades Computadas

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,19 +1,28 @@
 <template>
   <h2>Counter</h2>
-  <p> {{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
 </template>
 
 <script>
 export default {
-        // name: "counter",
-        data() {
-            return {
-                counter: 5
-            }
-        },
-}
+  // name: "counter",
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter * this.counter;
+    },
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
