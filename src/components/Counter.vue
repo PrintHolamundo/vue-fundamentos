60dd7fe10d5f03cd7b04e90aefe857fe9b1d71e7
PrintHolamundo <jdohca@gmail.com>
2022-03-12 09:13:22 -0600

    incrementar y decrementar

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,10 @@
 <template>
   <h2>Counter</h2>
   <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+  <div>
+    <button @click="counter--">-1</button>
+    <button @click="counter++">+1</button>
+  </div>
 </template>
 
 <script>
@@ -24,5 +28,24 @@ export default {
 };
 </script>
 
+
 <style>
+
+button{
+ background-color: #64bb87;
+ border-radius: 5px;
+ border: 1px solid #fff;
+ color: #fff;
+ cursor: pointer;
+ margin: 0 5px;
+ padding: 5px 15px;
+ transition: 0,3s ease-in-out;
+}
+
+button:hover{
+  background-color: #fff;
+  color: #64bb87;
+  border: 1px solid #64bb87;
+}
+
 </style>
